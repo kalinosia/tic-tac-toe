fff05b03733b0c307ec459e4dbc72d0872346ff9
kalinosia <kalinowska953@gmail.com>
2021-08-24 13:14:59 +0200

    Better look..?

--- tictactoe.py
+++ tictactoe.py
@@ -54,15 +54,15 @@ def actions(board):
     # function should return a set of all of the possible actions that can be taken on a given board.
     my_set = set()
 
-    i=0
-    j=0
+    i = 0
+    j = 0
     for row in board:
         for value in row:
             if value == EMPTY:  # Possible moves are any cells on the board that do not already have an X or an O in them.
                 my_set.add((i, j))  # Each action should be represented as a tuple (i, j) where i corresponds to the row...
-            j+=1
-        i+=1
-        j=0
+            j += 1
+        i += 1
+        j = 0
 
     return my_set
     # raise NotImplementedError
@@ -96,7 +96,7 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
     # The winner function should accept a board as input, and return the winner of the board if there is one.
-    have_winner = False
+    # have_winner = False
 
     if board[0][0] == O and board[0][1] == O and board[0][2] == O:
         return O
@@ -141,12 +141,12 @@ def terminal(board):
     Returns True if game is over, False otherwise.
     """
     if winner(board) is None:
-        #all cells have been filled without anyone winning, the function should return True.
+        # all cells have been filled without anyone winning, the function should return True.
         if len(actions(board)) == 0:
             return True
         else:
             return False
-    #If the game is over, either because someone has won the game
+    # If the game is over, either because someone has won the game
     if winner(board) == O or winner(board) == X:
         return True
     # raise NotImplementedError
@@ -156,18 +156,34 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    #If X has won the game, the utility is 1
-    if winner(board)==X:
+    # If X has won the game, the utility is 1
+    if winner(board) == X:
         return 1
     # f O has won the game, the utility is -1
-    if winner(board)==O:
+    if winner(board) == O:
         return -1
-    #If the game has ended in a tie, the utility is 0.
-    #You may assume utility will only be called on a board if terminal(board) is True.
+    # If the game has ended in a tie, the utility is 0.
+    # You may assume utility will only be called on a board if terminal(board) is True.
     else:
         return 0
 
-    #raise NotImplementedError
+
+def max_value(state):
+    v = -math.inf
+    if terminal(state):
+        return utility(state)
+    for actione in actions(state):
+        v = max(v, min_value(result(state, actione)))
+    return v
+
+
+def min_value(state):
+    v = +math.inf
+    if terminal(state):
+        return utility(state)
+    for actione in actions(state):
+        v = min(v, max_value(result(state, actione)))
+    return v
 
 
 def minimax(board):
@@ -177,25 +193,39 @@ def minimax(board):
     '''
     The minimax function should take a board as input, and return the optimal move for the player to move on that board.
 
-    The move returned should be the optimal action (i, j) that is one of the allowable actions on the board. If multiple moves are equally optimal, any of those moves is acceptable.
+    The move returned should be the optimal action (i, j) that is one of the allowable actions on the board. 
+    If multiple moves are equally optimal, any of those moves is acceptable.
     If the board is a terminal board, the minimax function should return None.
+..........
+Given a state s
+    The maximizing player picks action a in Actions(s) that produces the highest value of Min-Value(Result(s, a)).
+    The minimizing player picks action a in Actions(s) that produces the lowest value of Max-Value(Result(s, a)).
     '''
-    def max_value(state):
-        v = -math.inf
-        if terminal(state):
-            return utility(state)
-        for actione in actions(state):
-            v = max(v, min_value(result(state, actione)))
-        return v
 
-    def min_value(state):
-        v = +math.inf
-        if terminal(state):
-            return utility(state)
-        for actione in actions(state):
-            v = min(v, max_value(result(state, actione)))
-        return v
+    if len(actions(board)) >= 8:  # if first move - random move
+        from_Set_to_list = list(actions(board))
+        returning_one_random_tuple = from_Set_to_list[random.randint(0, len(actions(board)) - 1)]
+        return returning_one_random_tuple
 
+    panel = copy.deepcopy(board)  # necessary?probably not
+
+    scores = []
+    if player(board) == X:
+        for action in actions(board):
+            value = min_value(result(panel, action))
+            scores.append((value, action))
+    else:
+        for action in actions(board):
+            value = max_value(result(panel, action))
+            scores.append((value, action))
+
+    if player(board) == X:
+        return max(scores)[1]
+    else:
+        return min(scores)[1]
+
+
+'''
     def print_board(board):
         for row in board:
             for value in row:
@@ -205,34 +235,4 @@ def minimax(board):
                     print(value, " ",end="")
             print()
         print()
-    '''
-Given a state s
-    The maximizing player picks action a in Actions(s) that produces the highest value of Min-Value(Result(s, a)).
-    The minimizing player picks action a in Actions(s) that produces the lowest value of Max-Value(Result(s, a)).
-    '''
-
-    if len(actions(board))>=8: #if first move - random move
-        from_Set_to_list = list(actions(board))
-        returning_one_random_tuple = from_Set_to_list[random.randint(0, len(actions(board)) - 1)]
-        return returning_one_random_tuple
-
-    panel = copy.deepcopy(board) #  necessary?probably not
-
-    scores=[]
-    if player(board) == X:
-        for action in actions(board):
-            value=min_value(result(panel, action))
-            scores.append((value,action))#####
-    else:
-        for action in actions(board):
-            value=max_value(result(panel, action))
-            scores.append((value, action))
-
-    if player(board) == X:
-        return max(scores)[1]
-    else:
-        return min(scores)[1]
-
-
-
-
+        '''
